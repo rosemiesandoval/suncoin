446838f056730036bfd2bd06701e140b92fc42e6
Your Namerosemiesandoval <rosemiesandoval@gmail.com>
2024-04-04 05:23:16 +0800

    live this please

--- truffle-config.js
+++ truffle-config.js
@@ -4,8 +4,14 @@ module.exports = {
 			host: "127.0.0.1",
 			port: 7545,
 			network_id: "*"
-		}
-	},solc: {
+		},
+		"live": {
+			network_id: 1,
+			host: "127.0.0.1",
+			port: 8546   // Different than the default below
+		  }
+	},
+	solc: {
 		optimizer: {
 			enabled: true,
 			runs: 200
